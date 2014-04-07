9c377cf389bdb08a57c9013d7c54b049ddaa04e3
Ármin <rmin@152.66.178.183>
2014-04-07 21:03:56 +0200

    a

--- tower_defense/src/tower_defense/Projectile.java
+++ tower_defense/src/tower_defense/Projectile.java
@@ -7,30 +7,39 @@ import java.util.List;
 public class Projectile extends MovableGameObject {
 	private static final long serialVersionUID = -3507357373445026085L;
 	
+	
+	
 	private Damage damage;
 	//private double AoE;
 	
 	public Projectile(double x, double y, double destx,double desty ) {
+		this.x=x;
+		this.y=y;
+		targetX=destx;
+		targetY=desty;
 		damage = new Damage();
 	}
 	
 	// lövedék tevékenysége
 	@Override
 	public final boolean action() throws IOException {
-		explode();
-		return true;
+		double distance = Math.sqrt((x-targetX)*(x-targetX)+(y-targetY)*(y-targetY));
+		if(distance<movementSpeed)
+		{
+			explode();
+			return true;
+		}
+		else
+		{
+			//mozog a cél felé!
+			return false;
+		}
 	}
 	
 	// lövedék robbanása
 	public void explode() throws IOException {
-		Console.println(this + ".explode()");
-		Console.indent();
-		
 		// megsebzendõ objektumok
 		List<GameObject> objectsToDamage = new ArrayList<GameObject>();
-		
-		Console.printlnMsg("Melyik objektumok vannak a lövedék AoE-jében?");
-		
 		// objektumok kérése a user-tõl
 		while (true) {
 			GameObject object = Console.getObjectFromUser();
