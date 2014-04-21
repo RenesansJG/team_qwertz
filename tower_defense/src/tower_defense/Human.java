cc4c7e5269e988e78cbc7de66bf5667361e1c0ce
Ármin <rmin@Armin>
2014-04-21 22:39:28 +0200

    a

--- tower_defense/src/tower_defense/Human.java
+++ tower_defense/src/tower_defense/Human.java
@@ -2,10 +2,15 @@ package tower_defense;
 
 public class Human extends Enemy {
 	private static final long serialVersionUID = 5166440464581339881L;
+	
+	private static double baseHP = 100;
+	private static double levelHP = 5;
+	private static double levelRes = 0.05;
+	private static double baseMove = 3;
 
-	public Human(double x, double y) {
-		this.x=x;
-		this.y=y;
+	protected Human(Node targetNode, int level) {
+		super(targetNode,baseMove, level);
+		hp = new HP(baseHP + level * levelHP, levelRes * level, levelRes * level, levelRes * level);
 	}
 	
 	// toString függvény kiíratáshoz
