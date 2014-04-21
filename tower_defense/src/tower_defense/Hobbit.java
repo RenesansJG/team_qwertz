39da0e6cdfacf80d7b3a07a19f0b485c13a19721
Ármin <rmin@Armin>
2014-04-21 22:37:46 +0200

    a

--- tower_defense/src/tower_defense/Hobbit.java
+++ tower_defense/src/tower_defense/Hobbit.java
@@ -2,10 +2,17 @@ package tower_defense;
 
 public class Hobbit extends Enemy {
 	private static final long serialVersionUID = -3408698338441606044L;
+	
+	private static double baseHP = 70;
+	private static double levelHP = 5;
+	private static double baseRes = 0.25;
+	private static double levelRes = 0.05;
+	private static double baseMove = 3;
+	private static double levelMove = 0;
 
-	public Hobbit(double x, double y) {
-		this.x=x;
-		this.y=y;
+	protected Hobbit(Node targetNode, int level) {
+		super(targetNode,baseMove + level * levelMove, level);
+		hp = new HP(baseHP + level * levelHP, 0, 0, baseRes + levelRes * level);
 	}
 	
 	// toString függvény kiíratáshoz
