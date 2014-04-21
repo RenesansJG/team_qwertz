cf6e5aa6cc6e3e1b9a61b3041c0cf0089800dbae
Ármin <rmin@Armin>
2014-04-21 22:52:05 +0200

    a

--- tower_defense/src/tower_defense/Trap.java
+++ tower_defense/src/tower_defense/Trap.java
@@ -2,6 +2,11 @@ package tower_defense;
 
 public abstract class Trap extends GameObject {
 	private static final long serialVersionUID = -1174807768236727847L;
-	
+
 	protected double range;
+
+	protected Trap(double x, double y, double range) {
+		super(x, y);
+		this.range=range;
+	}
 }
