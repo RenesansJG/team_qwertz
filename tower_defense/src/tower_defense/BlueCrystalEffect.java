0a3f3ae7d6f5b4b18bb6a70eb7eb9fae1d5ece0d
Ármin <rmin@152.66.178.183>
2014-04-23 01:56:02 +0200

    kiíratás

--- tower_defense/src/tower_defense/BlueCrystalEffect.java
+++ tower_defense/src/tower_defense/BlueCrystalEffect.java
@@ -12,6 +12,7 @@ public class BlueCrystalEffect extends Effect {
 	public void apply(Tower tower) {
 		// attackspeed növelés
 		tower.modifyAttackspeedMultiplier(1);
+		Console.println(this + ": " + tower + " attack speed-je növelve");
 	}
 	
 	// effekt alkalmazása lassító csapdán
@@ -19,15 +20,18 @@ public class BlueCrystalEffect extends Effect {
 	public void apply(SlowTrap slowTrap) {
 		// lassítás növelés
 		slowTrap.modifySlowMultiplier(1);
+		Console.println(this + ": " + slowTrap + " slow effect-je növelve");
 	}
 	
 	@Override
 	public void restore(Tower tower) {
 		tower.modifyAttackspeedMultiplier(-1);
+		Console.println(this + ": " + tower + " attack speed-je visszaállítva");
 	}
 	
 	public void restore(SlowTrap slowTrap) {
 		slowTrap.modifySlowMultiplier(-1);
+		Console.println(this + ": " + slowTrap + " slow effect-je visszaállítva");
 	}
 	
 	// toString függvény kiíratáshoz
